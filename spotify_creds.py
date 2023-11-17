5200e9f6ec5e9f0fcd9d1d0cad618f3e0dcb634d
smznahid <sheikh.nahid0802@gmail.com>
2023-11-17 21:41:42 +0000

    init credsloader module

---
+++ spotify_creds.py
@@ -0,0 +1,7 @@
+import json
+from spotipy.oauth2 import SpotifyClientCredentials
+class CredentialsLoader:
+    def __init__(self):
+        #TODO: complete implementation of CredentialsLoader
+        pass
+    
